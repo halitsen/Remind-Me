965ed7f47f108bbdc145d66c04ec67c7c0ac1009
Halit Sen <sen.halitt@gmail.com>
2019-12-15 12:37:37 +0300

    reminder database created

---
+++ app/src/main/java/halit/sen/remindme/database/ReminderData.kt
@@ -0,0 +1,33 @@
+package halit.sen.remindme.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+
+@Entity(tableName = "reminder_table")
+class ReminderData : Serializable{
+
+    @PrimaryKey(autoGenerate = true)
+    var reminderId: Long = 0L
+
+    @ColumnInfo(name = "title")
+    var reminderTitle:String = "Remind Me"
+
+    @ColumnInfo(name = "content")
+    var reminderContent:String = ""
+
+    @ColumnInfo(name = "created_date")
+    var creaTedDate:Long = System.currentTimeMillis()
+
+    @ColumnInfo(name = "is_birthday")
+    var isBirthday: Boolean = false
+
+    @ColumnInfo(name = "notify_time")
+    var notifyTime:Long = 0L
+
+    @ColumnInfo(name = "is_active")
+    var isActive: Boolean = false
+
+}
